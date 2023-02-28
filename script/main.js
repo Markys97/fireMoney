da1156d89dc1ee2bcb970bb63c2b2323e8f3826f
brexe junior <brexejunior@gmail.com>
2023-02-28 16:10:42 +0300

    second day work

--- script/main.js
+++ script/main.js
@@ -25,3 +25,55 @@ slideRanges.forEach(slideRange=>{
         }
     });
 })
+
+let getMoneyRowElt= document.querySelector('.get-money__row')
+let getMoneyRowEltHeiht=parseFloat(getComputedStyle(getMoneyRowElt).height)
+
+
+let elt= document.querySelector('.get-money__item')
+let eltHeigt= parseFloat(getComputedStyle(elt).height)
+
+let styleElt= document.createElement('style')
+document.head.append(styleElt)
+let cssText
+
+if(screen.width <768){
+     cssText=`.get-money__row::after{height:${(0)+(eltHeigt-3)}px}`// getMoneyRowEltHeiht*3/4
+    styleElt.innerText= cssText
+}
+
+
+let getMoneyIcons= document.querySelectorAll('.get-money__icon')
+
+getMoneyIcons.forEach((getMoneyIcon,index,arr)=> {
+    let lastIndex = arr.length-1
+    getMoneyIcon.addEventListener('click',function(e){
+       arr.forEach(elt=> elt.closest('.get-money__item').classList.remove('active'))
+      for(let i=0; i<=index; i++){
+        arr[i].closest('.get-money__item').classList.add('active')
+      }
+
+      if(screen.width <768){
+        if(index===0){
+            cssText=`.get-money__row::after{height:${(0)+(eltHeigt-3)}px}`
+            styleElt.innerText= cssText
+            return false
+        }
+    
+          if(index==lastIndex){
+            cssText=`.get-money__row::after{height:${(100)}%`
+            styleElt.innerText= cssText
+            return false
+        }
+    
+        cssText=`.get-money__row::after{height:${(getMoneyRowEltHeiht*(index)/4)+(eltHeigt-3)}px}`
+        styleElt.innerText= cssText
+      
+      }
+
+      if(screen.width>= 768){
+
+      }
+     
+    })
+})
