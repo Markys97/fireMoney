bc455cebd73a94b499021ff1bdb27a9d0e8f7c13
brexe junior <brexejunior@gmail.com>
2023-03-01 22:34:08 +0300

    end beta v2

--- script/main.js
+++ script/main.js
@@ -1,7 +1,9 @@
 let slideRanges = document.querySelectorAll('.formulation__range-slider.slider-range');
+let sliderMoney = document.querySelector('.input-range__slide--money')
+let sliderDuration = document.querySelector('.input-range__slide--duration')
 
 slideRanges.forEach(slideRange=>{
-    
+   
 
     noUiSlider.create(slideRange, {
         start: [20],
@@ -24,59 +26,55 @@ slideRanges.forEach(slideRange=>{
             }
         }
     });
-})
-
-let getMoneyRowElt= document.querySelector('.get-money__row')
-let getMoneyRowEltHeiht=parseFloat(getComputedStyle(getMoneyRowElt).height)
-
-
-let elt= document.querySelector('.get-money__item')
-let eltHeigt= parseFloat(getComputedStyle(elt).height)
-
-let styleElt= document.createElement('style')
-document.head.append(styleElt)
-let cssText
-
-if(screen.width <768){
-     cssText=`.get-money__row::after{height:${(0)+(eltHeigt-3)}px}`// getMoneyRowEltHeiht*3/4
-    styleElt.innerText= cssText
-}
-
-
-let getMoneyIcons= document.querySelectorAll('.get-money__icon')
-
-getMoneyIcons.forEach((getMoneyIcon,index,arr)=> {
-    let lastIndex = arr.length-1
-    getMoneyIcon.addEventListener('click',function(e){
-       arr.forEach(elt=> elt.closest('.get-money__item').classList.remove('active'))
-      for(let i=0; i<=index; i++){
-        arr[i].closest('.get-money__item').classList.add('active')
-      }
-
-      if(screen.width <768){
-        if(index===0){
-            cssText=`.get-money__row::after{height:${(0)+(eltHeigt-3)}px}`
-            styleElt.innerText= cssText
-            return false
+    slideRange.noUiSlider.on('slide',function(value){
+        parseInt(slideRange.dataset.max)
+        if(parseInt(slideRange.dataset.max)=== parseInt(value)){
+           slideRange.querySelector('.noUi-tooltip').classList.add('prevent-end')
+        }else{
+            slideRange.querySelector('.noUi-tooltip').classList.remove('prevent-end')
         }
-    
-          if(index==lastIndex){
-            cssText=`.get-money__row::after{height:${(100)}%`
-            styleElt.innerText= cssText
-            return false
-        }
-    
-        cssText=`.get-money__row::after{height:${(getMoneyRowEltHeiht*(index)/4)+(eltHeigt-3)}px}`
-        styleElt.innerText= cssText
-      
-      }
-
-      if(screen.width>= 768){
-
-      }
-     
     })
 })
+
+// ranges header-fixed
+noUiSlider.create(sliderMoney, {
+    start: [20000],
+    connect: 'lower',
+    range: {
+        'min': 0,
+        'max': 45000
+    },
+   
+});
+// sliderMoney.noUiSlider.get()
+sliderMoney.closest('.input-range').querySelector('.input-range__data-number').innerText=parseInt(sliderMoney.noUiSlider.get())
+
+noUiSlider.create(sliderDuration, {
+    start: [10],
+    connect: 'lower',
+    range: {
+        'min': 1,
+        'max': 20
+    },
+   
+});
+sliderDuration.closest('.input-range').querySelector('.input-range__data-number').innerText=parseInt(sliderDuration.noUiSlider.get())
+
+// set value of money
+sliderMoney.noUiSlider.on('slide',function(value){
+    console.log(value)
+    sliderMoney.closest('.input-range').querySelector('.input-range__data-number').innerText=parseInt(value)
+})
+// set value of time
+sliderDuration.noUiSlider.on('slide',function(value){
+    console.log(value)
+    sliderDuration.closest('.input-range').querySelector('.input-range__data-number').innerText=parseInt(value)
+})
+
+
+
+
+
 
 // open/close answer faq
 let faqItems= [...document.querySelectorAll('.item-faq')];
@@ -88,3 +86,93 @@ faqItems.forEach((faqItem,index,arr)=>{
         console.log(answer.clientHeight)
     })
 })
+
+
+// handler header-fixed view
+let headerFixed= document.querySelector('.header-fixed')
+
+let headerFixed_height=parseFloat(getComputedStyle(headerFixed).height);
+headerFixed.style.top=`-${headerFixed_height}px`
+window.addEventListener('scroll',function(e){
+    
+    if(this.scrollY>headerFixed_height ){
+        headerFixed.classList.add('active')
+    }else{
+        headerFixed.classList.remove('active')
+    }
+
+    console.log(this.scrollY,headerFixed_height)
+})
+
+
+// handler step get money
+let style= document.createElement('style');
+let getMoneyRow =  document.querySelector('.get-money__row')
+let getMoneyItems= document.querySelectorAll('.get-money__item')
+let getMoneyItemActivePosition= null;
+
+const pxToPercent=(value,total)=>{
+    return (value*100)/total
+}
+const setStepDesktop=(position,arrLenght)=>{
+    let getMoneyRow_width= parseFloat(getComputedStyle(getMoneyRow).width)
+    let getMoneyRow_height= parseFloat(getComputedStyle(getMoneyRow).height)
+
+    let value=getMoneyRow_width*(position*2-1)/8;
+    let total= getMoneyRow_width
+    let finalValue= pxToPercent(value,total)
+    let cssText
+    if(screen.width>= 768){
+        if(position === arrLenght){
+            let cssText= `.get-money__row::after{width:${100}%}`
+            style.innerHTML=cssText
+            document.head.append(style)
+    
+            return
+        }
+         cssText= `.get-money__row::after{width:${finalValue}%}`
+    }else{
+        if(position === arrLenght){
+            let cssText= `.get-money__row::after{height:${100}%}`
+            style.innerHTML=cssText
+            document.head.append(style)
+    
+            return
+        }
+         cssText= `.get-money__row::after{height:${finalValue}%}`
+    }
+    style.innerHTML=cssText
+    document.head.append(style)
+}
+// get index active item
+getMoneyItems.forEach((item,index,arr)=>{
+     if(item.classList.contains('active')){
+        getMoneyItemActivePosition=index+1
+     }
+     item.querySelector('.get-money__icon').addEventListener('click',function(e){
+        getMoneyItemActivePosition=index+1
+        arr.forEach(elt=>{
+            elt.classList.remove('active')
+          
+        })
+        for(let i=0;i<=index;i++){
+            arr[i].classList.add('active')
+        }
+        setStepDesktop(getMoneyItemActivePosition,getMoneyItems.length)
+
+    })
+})
+window.addEventListener('resize',function(e){
+    setStepDesktop(getMoneyItemActivePosition,getMoneyItems.length)
+
+})
+
+ 
+
+//set first step
+setStepDesktop(getMoneyItemActivePosition,getMoneyItems.length)
+
+
+
+
+
